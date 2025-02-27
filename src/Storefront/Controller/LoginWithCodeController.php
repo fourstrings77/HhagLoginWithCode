c1587a174bcc6ab3d9b0e41e78617e013c3890a1
fourstrings77 <hagedorn.hauke@gmx.de>
2025-02-27 11:51:14 +0100

    add storefront page, controller and template

--- src/Storefront/Controller/LoginWithCodeController.php
+++ src/Storefront/Controller/LoginWithCodeController.php
@@ -2,15 +2,34 @@
 
 namespace HhagLoginWithCode\Storefront\Controller;
 
-use http\Params;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
+use Shopware\Storefront\Page\Account\Login\AccountLoginPageLoader;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LoginWithCodeController extends StorefrontController
 {
+    protected AccountLoginPageLoader $accountLoginPageLoader;
+    public function __construct(AccountLoginPageLoader $accountLoginPageLoader)
+    {
+        $this->accountLoginPageLoader = $accountLoginPageLoader;
+    }
+
     #[Route(path: '/request-code', name: 'frontend.account.login.request_code', defaults: ['_routeScope' => ['storefront']], methods: ['GET'])]
-    public function requestCodeForm() : Response{
+    public function requestCodeForm(Request $request, SalesChannelContext $context) : Response{
+
+        $page = $this->accountLoginPageLoader->load($request, $context);
+
+        return $this->renderStorefront('@Storefront/storefront/page/account/login/request-code.html.twig', [
+            'redirectTo' => '',
+            'page' => $page
+        ]);
+    }
+
+    #[Route(path: '/verify-code', name: 'frontend.account.login.verify_code', defaults: ['_routeScope' => ['storefront']], methods: ['GET'])]
+    public function verfify() : Response{
         dd('test');
     }
 }
---
+++ src/Storefront/Page/LoginWithCodePage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace HhagLoginWithCode\Storefront\Page;
+
+use Shopware\Storefront\Page\GenericPageLoader;
+
+class LoginWithCodePage extends GenericPageLoader
+{
+
+}
