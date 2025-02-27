93d228217fe31309548eab752a5515ccbec88a83
fourstrings77 <hagedorn.hauke@gmx.de>
2025-02-27 14:48:48 +0100

    add custom mailtemplate with type

--- src/Migration/Migration1740659824AddType.php
+++ src/Migration/Migration1740659824AddType.php
@@ -54,8 +54,114 @@ class Migration1740659824AddType extends MigrationStep
         return $typeId;
     }
 
-    public function createMailTemplate(): void
+    public function createMailTemplate(Connection $connection, $mailTemplateTypeId): void
     {
+        $mailTemplateId = Uuid::randomHex();
 
+        $enGbLangId = $this->getLanguageByLocale($connection, 'en-GB');
+        $deDeLangId = $this->getLanguageByLocale($connection, 'de-DE');
+
+        $connection->executeStatement("
+        INSERT IGNORE INTO `mail_template`
+            (id, mail_template_type_id, system_default, created_at)
+        VALUES
+            (:id, :mailTemplateTypeId, :systemDefault, :createdAt)
+        ",[
+            'id' => Uuid::fromHexToBytes($mailTemplateId),
+            'mailTemplateTypeId' => Uuid::fromHexToBytes($mailTemplateTypeId),
+            'systemDefault' => 0,
+            'createdAt' => (new DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+        ]);
+
+        if (!empty($enGbLangId)) {
+            $connection->executeStatement("
+            INSERT IGNORE INTO `mail_template_translation`
+                (mail_template_id, language_id, sender_name, subject, description, content_html, content_plain, created_at)
+            VALUES
+                (:mailTemplateId, :languageId, :senderName, :subject, :description, :contentHtml, :contentPlain, :createdAt)
+            ",[
+                'mailTemplateId' => Uuid::fromHexToBytes($mailTemplateId),
+                'languageId' => $enGbLangId,
+                'senderName' => '{{ salesChannel.name }}',
+                'subject' => 'Example mail template subject',
+                'description' => 'Example mail template description',
+                'contentHtml' => $this->getContentHtmlEn(),
+                'contentPlain' => $this->getContentPlainEn(),
+                'createdAt' => (new DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+            ]);
+        }
+
+        if (!empty($deDeLangId)) {
+            $connection->executeStatement("
+            INSERT IGNORE INTO `mail_template_translation`
+                (mail_template_id, language_id, sender_name, subject, description, content_html, content_plain, created_at)
+            VALUES
+                (:mailTemplateId, :languageId, :senderName, :subject, :description, :contentHtml, :contentPlain, :createdAt)
+            ",[
+                'mailTemplateId' => Uuid::fromHexToBytes($mailTemplateId),
+                'languageId' => $deDeLangId,
+                'senderName' => '{{ salesChannel.name }}',
+                'subject' => 'Dein Logincode für {{ salesChannel.name }}',
+                'description' => 'Basistemplate für den Login-Code',
+                'contentHtml' => $this->getContentHtmlDe(),
+                'contentPlain' => $this->getContentPlainDe(),
+                'createdAt' => (new DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+            ]);
+        }
+
+    }
+
+    private function getContentHtmlEn(): string
+    {
+        return <<<MAIL
+        <div style="font-family:arial; font-size:12px;">
+            <p>
+                Example HTML content!
+            </p>
+        </div>
+        MAIL;
+    }
+
+    private function getContentPlainEn(): string
+    {
+        return <<<MAIL
+        Example plain content!
+        MAIL;
+    }
+
+    private function getContentHtmlDe(): string
+    {
+        return <<<MAIL
+        <div style="font-family:arial; font-size:12px;">
+            <p>
+                Beispiel HTML Inhalt!
+            </p>
+        </div>
+        MAIL;
+    }
+
+    private function getContentPlainDe(): string
+    {
+        return <<<MAIL
+        Beispiel Plain Inhalt!
+        MAIL;
+    }
+
+    public function getLanguageByLocale(Connection $connection, string $locale): ?string
+    {
+        $sql = <<<SQL
+        SELECT `language`.`id`
+        FROM `language`
+        INNER JOIN `locale` ON `locale`.`id` = `language`.`locale_id`
+        WHERE `locale`.`code` = :code
+        SQL;
+
+        $languageId = $connection->executeQuery($sql, ['code' => $locale])->fetchOne();
+
+        if (empty($languageId)) {
+            return null;
+        }
+
+        return $languageId;
     }
 }
